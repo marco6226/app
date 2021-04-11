6473b1511c4ccddec990f9055119b35e21987062
Marco_mac <marco_haz24@hotmail.com>
2021-04-10 23:55:37 -0500

    ajustes id tienda

--- src/app/modulos/com/components/login/login.component.ts
+++ src/app/modulos/com/components/login/login.component.ts
@@ -360,7 +360,7 @@ export class LoginComponent implements OnInit {
     if (so == 'android') {
       this.market.open('co.sigess.app');
     } else if (so == 'ios') {
-      this.market.open('1473016574');
+      this.market.open('1562431304');
     } else {
       alert("Plataforma no detectada");
     }
